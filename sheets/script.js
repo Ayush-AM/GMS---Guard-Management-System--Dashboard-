be5eb65a3eb87716c3b011475f6b9c817245d901
Ayush Mahajan <140263932+Ayush-AM@users.noreply.github.com>
2024-12-07 09:34:03 +0530

    updated sheets

--- sheets/script.js
+++ sheets/script.js
@@ -1,13 +1,13 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz7hsG-0oULjtOxDlZPwZAx5_CFYH1SaMb2DBkH_l2dkddKdH0DzfF2UYkrsB9rUV15/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzabrQiNbGfWyUwacV1OTTVmL-AEfMh4xLqbCmUoVWid0z4ewjZufd3DewgCpsy5uGK2g/exec'
+
 
 const form = document.forms['contact-form']
 
+
 form.addEventListener('submit', e => {
-  
   e.preventDefault()
-  
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! Form is submitted" ))
+  .then(response => alert("Thank you! your form is submitted successfully." ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
